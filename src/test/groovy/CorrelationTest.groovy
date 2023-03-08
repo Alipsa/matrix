71652a46ace6d987e265dd05aca0502034ff04f9
per <per@alipsa.se>
2023-03-08 18:12:16 +0100

    add kendall correlation statistic

--- src/test/groovy/CorrelationTest.groovy
+++ src/test/groovy/CorrelationTest.groovy
@@ -1,4 +1,6 @@
+import org.apache.commons.math3.stat.correlation.KendallsCorrelation
 import org.junit.jupiter.api.Test
+import se.alipsa.matrix.Correlation
 
 import java.math.RoundingMode;
 
@@ -28,5 +30,15 @@ class CorrelationTest {
     assertEquals(
         0.45643546458764,
         cor([15, 18, 21, 15, 21], [25, 25, 27, 27, 27], SPEARMAN).setScale(14, RoundingMode.HALF_EVEN))
+  }
+
+  @Test
+  void testKendallsCorrelation() {
+    def x1 = [12, 2, 1, 12, 2]
+    def x2 = [1, 4, 7, 1, 0]
+    def kc = new KendallsCorrelation()
+    def apacheCor = kc.correlation(x1 as double[], x2 as double[])
+    assertEquals(-0.47140452079103173, apacheCor)
+    assertEquals(apacheCor, corKendall(x1, x2))
   }
 }
