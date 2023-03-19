3ce917f530a283aa697d901c27571a248b42761e
per <per@alipsa.se>
2023-03-19 22:53:09 +0100

    add string to localDAteTime converter

--- src/main/groovy/se/alipsa/groovy/matrix/ListConverter.groovy
+++ src/main/groovy/se/alipsa/groovy/matrix/ListConverter.groovy
@@ -2,6 +2,7 @@ package se.alipsa.groovy.matrix
 
 import java.text.NumberFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class ListConverter {
@@ -36,6 +37,30 @@ class ListConverter {
     dates.eachWithIndex { String d, int i ->
       try {
         dat.add(LocalDate.parse(d, formatter))
+      } catch (Exception e) {
+        throw new ConversionException("Failed to convert $d to LocalDate in index $i", e)
+      }
+    }
+    return dat
+  }
+
+  static List<LocalDateTime> toLocalDateTimes(String[] dates) {
+    def dat = new ArrayList<LocalDateTime>(dates.length)
+    dates.eachWithIndex { String d, int i ->
+      try {
+        dat.add(LocalDateTime.parse(d))
+      } catch (Exception e) {
+        throw new ConversionException("Failed to convert $d to LocalDateTime in index $i", e)
+      }
+    }
+    return dat
+  }
+
+  static List<LocalDateTime> toLocalDateTimes(String[] dates, DateTimeFormatter formatter) {
+    def dat = new ArrayList<LocalDateTime>(dates.length)
+    dates.eachWithIndex { String d, int i ->
+      try {
+        dat.add(LocalDateTime.parse(d, formatter))
       } catch (Exception e) {
         throw new ConversionException("Failed to convert $d to LocalDate in index $i", e)
       }
