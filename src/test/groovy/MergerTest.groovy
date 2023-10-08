20c2e38b9eca832980dc82805ecbbb7c92c38e41
pernyf <per.nyfelt@nordnet.se>
2023-10-08 19:32:59 +0200

    Implement a primitive join function

---
+++ src/test/groovy/MergerTest.groovy
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Test
+import se.alipsa.groovy.matrix.Matrix
+import se.alipsa.groovy.matrix.Merger
+
+import static org.junit.jupiter.api.Assertions.*
+
+class MergerTest {
+
+    @Test
+    void testInnerJoin() {
+        def e = new Matrix('employees', [
+                id: 1..5,
+                firstName:["Rick","Dan","Michelle","Ryan","Gary"],
+                salary:[623.3,515.2,611.0,729.0,843.25],
+                startDate: ["2012-01-01", "2013-09-23", "2014-11-15", "2014-05-11", "2015-03-27"]
+        ])
+
+        def f = new Matrix('eln', [
+                id: 1..5,
+                lastName: ["Smith", "Carpenter", "Bowman", "Carson", "McDougal"]
+        ])
+
+        def merged = Merger.merge(e, f, 'id')
+        //println merged.toMarkdown()
+        assertIterableEquals([1, 'Rick', 623.3, '2012-01-01', 'Smith'], merged.row(0))
+        assertIterableEquals([5, 'Gary', 843.25, '2015-03-27', 'McDougal'], merged.row(4))
+
+
+        // Make sure the original matrices was unaltered
+        assertEquals(new Matrix('employees', [
+                id: 1..5,
+                firstName:["Rick","Dan","Michelle","Ryan","Gary"],
+                salary:[623.3,515.2,611.0,729.0,843.25],
+                startDate: ["2012-01-01", "2013-09-23", "2014-11-15", "2014-05-11", "2015-03-27"]
+        ]), e)
+    }
+
+    @Test
+    void testLeftJoin(){
+        def e = new Matrix('employees', [
+                id: 1..5,
+                firstName:["Rick","Dan","Michelle","Ryan","Gary"],
+                salary:[623.3,515.2,611.0,729.0,843.25],
+                startDate: ["2012-01-01", "2013-09-23", "2014-11-15", "2014-05-11", "2015-03-27"]
+        ])
+        def g = new Matrix('eln', [
+                employeeId: 2..4,
+                lastName: ["Carpenter", "Bowman", "Carson"]
+        ])
+        def leftJoined = Merger.merge(e, g, [x: 'id', y:'employeeId'], true)
+        //println leftJoined.toMarkdown()
+        assertEquals([1, 'Rick', 623.3, '2012-01-01', null], leftJoined.row(0))
+        assertEquals([2, 'Dan', 515.2, '2013-09-23', 'Carpenter'], leftJoined.row(1))
+        assertEquals([5, 'Gary', 843.25, '2015-03-27', null], leftJoined.row(4))
+
+        // Make sure the original matrices was unaltered
+        assertEquals(new Matrix('employees', [
+                id: 1..5,
+                firstName:["Rick","Dan","Michelle","Ryan","Gary"],
+                salary:[623.3,515.2,611.0,729.0,843.25],
+                startDate: ["2012-01-01", "2013-09-23", "2014-11-15", "2014-05-11", "2015-03-27"]
+        ]), e)
+        assertEquals(new Matrix('eln', [
+                employeeId: 2..4,
+                lastName: ["Carpenter", "Bowman", "Carson"]
+        ]), g)
+    }
+}
