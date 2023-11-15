70c1a4fad9eb52578e3940c6ca098d9d43972bac
per <per@alipsa.se>
2023-11-15 13:45:59 +0100

    added close step

--- matrix-core/release.sh
+++ matrix-core/release.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 source ~/.sdkman/bin/sdkman-init.sh
 source jdk17
-./gradlew clean build publishMavenPublicationToMavenRepository || exit 1
+./gradlew clean build release || exit 1
 PROJECT=$(basename "$PWD")
 if grep "version '" build.gradle | grep -q 'SNAPSHOT'; then
   echo "$PROJECT snapshot published"
 else
   echo "$PROJECT uploaded, release it if it checks out"
   echo "see https://central.sonatype.org/publish/release/ for more info"
-  browse https://oss.sonatype.org
+  browse https://oss.sonatype.org &
 fi
