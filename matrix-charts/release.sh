181d845e890c04d6972cb1f4039552837c5681a4
per <per@alipsa.se>
2025-02-16 18:41:46 +0100

    release matrix-charts 0.1

---
+++ matrix-charts/release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+source ~/.sdkman/bin/sdkman-init.sh
+source jdk17
+#./gradlew clean publishToSonatype closeAndReleaseSonatypeStagingRepository
+../gradlew :matrix-charts:clean :matrix-charts:build :matrix-charts:release || exit 1
+PROJECT=$(basename "$PWD")
+if grep "version '" build.gradle | grep -q 'SNAPSHOT'; then
+  echo "$PROJECT snapshot published"
+else
+  echo "$PROJECT uploaded and released"
+  echo "see https://central.sonatype.org/publish/release/ for more info"
+  # browse https://oss.sonatype.org &
+fi
