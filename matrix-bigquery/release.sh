fd9e8402f4714dda9ee20a1f2ebccfb5bf243cca
per <per@alipsa.se>
2025-02-16 18:36:30 +0100

    release matrix-bigquery 0.1

---
+++ matrix-bigquery/release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+source ~/.sdkman/bin/sdkman-init.sh
+source jdk17
+#./gradlew clean publishToSonatype closeAndReleaseSonatypeStagingRepository
+../gradlew :matrix-bigquery:clean :matrix-bigquery:build :matrix-bigquery:release || exit 1
+PROJECT=$(basename "$PWD")
+if grep "version '" build.gradle | grep -q 'SNAPSHOT'; then
+  echo "$PROJECT snapshot published"
+else
+  echo "$PROJECT uploaded, release it if it checks out"
+  echo "see https://central.sonatype.org/publish/release/ for more info"
+  # browse https://oss.sonatype.org &
+fi
