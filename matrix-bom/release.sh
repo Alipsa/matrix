fdcb348ac44b1fe76f871a8e1c45fba9382ee826
per <per@alipsa.se>
2025-02-21 15:04:10 +0100

    release bom 1.1.2, upgrade to latest nexus release plugin

--- matrix-bom/release.sh
+++ matrix-bom/release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-localRepo=$(mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
+localRepo=$(mvn -f bom.xml help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
 if [[ -d "$localRepo/se/alipsa/matrix" ]]; then
   echo "if the artifacts are not available in central we cannot release, so"
   echo "removing local cache in $localRepo/se/alipsa/matrix"
