927c9e3b09f8eabc9314ed5b43aa26f9b7ef5584
pernyf <per.nyfelt@nordnet.se>
2025-03-04 12:12:24 +0100

    Heatmap now working

--- matrix-xchart/src/main/groovy/se/alipsa/matrix/xchart/HeatmapChart.groovy
+++ matrix-xchart/src/main/groovy/se/alipsa/matrix/xchart/HeatmapChart.groovy
@@ -7,9 +7,12 @@ import org.knowm.xchart.style.HeatMapStyler
 import se.alipsa.matrix.core.Column
 import se.alipsa.matrix.core.ListConverter
 import se.alipsa.matrix.core.Matrix
+import se.alipsa.matrix.core.MatrixBuilder
 import se.alipsa.matrix.xchart.abstractions.AbstractChart
 
 class HeatmapChart extends AbstractChart<HeatmapChart, HeatMapChart, HeatMapStyler, HeatMapSeries> {
+
+  Matrix heatMapMatrix
 
   private HeatmapChart(Matrix matrix, Integer width = null, Integer height = null) {
     super.matrix = matrix
@@ -32,18 +35,25 @@ class HeatmapChart extends AbstractChart<HeatmapChart, HeatMapChart, HeatMapStyl
     new HeatmapChart(matrix, width, height)
   }
 
-  HeatmapChart addSeries(String seriesName, String redCol, String greenCol, String blueCol) {
-    addSeries(seriesName, matrix[redCol], matrix[greenCol], matrix[blueCol])
+  HeatmapChart addSeries(String seriesName, String colName, Integer columns = null) {
+    addSeries(seriesName, matrix[colName], columns)
   }
 
-  HeatmapChart addSeries(String seriesName, Column red, Column green, Column blue) {
+  HeatmapChart addSeries(String seriesName, Column col, Integer columns = null) {
+    int nCols = columns ?: (Math.sqrt(col.size() as int))
+    int nRows = (int) (col.size() / nCols)
     List<Number[]> heatData = []
-    int nrows = red.size()
-    for (int r = 0; r < nrows; r++) {
-      heatData << ([red[r], green[r], blue[r]] as Number[])
+    Number[] numberArray = new Number[]{}
+    def tmpRows = []
+    for (int r = 0; r < nRows; r++) {
+      for (int c = 0; c < nCols; c++) {
+        heatData << [r, c, col[r+c]].toArray(numberArray)
+        tmpRows << [r, c, col[r+c]]
+      }
     }
-    def xData = 1..nrows
-    def yData = 1..3
+    def xData = 1..nRows
+    def yData = 1..nCols
+    heatMapMatrix = new MatrixBuilder().rows(tmpRows).build()
     xchart.addSeries(seriesName, xData, yData, heatData)
     this
   }
--- matrix-xchart/src/test/groovy/test/alipsa/matrix/xchart/HeatmapChartTest.groovy
+++ matrix-xchart/src/test/groovy/test/alipsa/matrix/xchart/HeatmapChartTest.groovy
@@ -4,52 +4,65 @@ import org.junit.jupiter.api.Test
 import org.knowm.xchart.BitmapEncoder
 import org.knowm.xchart.HeatMapChart
 import org.knowm.xchart.HeatMapChartBuilder
+import org.knowm.xchart.style.Styler
+import org.knowm.xchart.style.theme.MatlabTheme
 import se.alipsa.matrix.core.Matrix
 import se.alipsa.matrix.core.MatrixBuilder
 import se.alipsa.matrix.xchart.HeatmapChart
+import se.alipsa.matrix.xchart.MatrixTheme
 
+import static org.junit.jupiter.api.Assertions.assertEquals
 import static org.junit.jupiter.api.Assertions.assertTrue
 
 class HeatmapChartTest {
 
   @Test
   void testHeatmap() {
+    def data = [123, 210, 89, 87, 245, 187, 100, 110, 99, 59, 110, 101]
     Matrix matrix = new MatrixBuilder().data(
-        'red': [123, 210, 89, 87],
-        'green': [245, 187, 100, 110],
-        'blue': [99, 59, 110, 101]
-    ).types([Number]*3)
+        'c': data
+    ).types(Number)
     .build()
 
-    println matrix.content()
-
-    def hc = HeatmapChart.create(matrix)
-      .addSeries('heatmap','red', 'green', 'blue')
+    def hc = HeatmapChart.create(matrix, 1000, 600)
+        .setTitle(getClass().getSimpleName())
+        .addSeries("Basic HeatMap",'c')
+    hc.style.setPlotContentSize(1)
+    hc.style.setShowValue(true)
     File file = new File("build/testHeatmap.png")
     hc.exportPng(file)
     assertTrue(file.exists())
-  }
+    Matrix hm = hc.heatMapMatrix
+    //println hm.content()
 
-  @Test
-  void testRandomHeatmap() {
+    // Construct a heatmap using XChart directly to compare
     HeatMapChart chart =
-        new HeatMapChartBuilder().width(1000).height(600).title(getClass().getSimpleName()).build();
+        new HeatMapChartBuilder().width(1000).height(600)
+            .title(getClass().getSimpleName()).build()
 
-    chart.getStyler().setPlotContentSize(1);
-    chart.getStyler().setShowValue(true);
+    chart.getStyler().theme = new MatrixTheme()
+    chart.getStyler().setPlotContentSize(1)
+    chart.getStyler().setShowValue(true)
+
 
     int[] xData = [1, 2, 3, 4]
     int[] yData = [1, 2, 3]
     int[][] heatData = new int[xData.length][yData.length]
-    Random random = new Random();
+    def rows = []
     for (int i = 0; i < xData.length; i++) {
+      def row = []
       for (int j = 0; j < yData.length; j++) {
-        heatData[i][j] = random.nextInt(1000)
+        heatData[i][j] = data[i+j]
+        rows << [i, j, data[i+j]]
       }
     }
+
+    Matrix m = new MatrixBuilder().rows(rows).build()
+    //println m.content()
     chart.addSeries("Basic HeatMap", xData, yData, heatData)
-    File file = new File("build/testRandomHeatmap.png")
+    file = new File("build/testHeatmap2.png")
     BitmapEncoder.saveBitmap(chart, file.absolutePath, BitmapEncoder.BitmapFormat.PNG)
     assertTrue(file.exists())
+    assertEquals(m, hm, m.diff(hm))
   }
 }
