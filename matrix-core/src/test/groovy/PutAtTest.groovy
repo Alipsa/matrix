431f592927c88abcbf9a03b63f7b089baa64085b
per <per@alipsa.se>
2025-04-14 18:34:38 +0200

    add more putAt(...) = null tests

--- matrix-core/src/test/groovy/PutAtTest.groovy
+++ matrix-core/src/test/groovy/PutAtTest.groovy
@@ -72,7 +72,7 @@ class PutAtTest {
     }
 
     def putAt(List list, Object value) {
-      println "putAt: $list : $value"
+      //println "putAt: $list : $value"
       Integer rowIdx = list[0] as Integer
       Integer colIdx = list[1] as Integer
       // Must add parenthesis for assignment to work
@@ -82,7 +82,7 @@ class PutAtTest {
     // This is never called but when compiling static it must exists for it to compile
     // [Static type checking] - Cannot find matching method PutAtTest$TwoD#getAt(java.util.List<E>). Please check if the declared type is correct and if the method exists.
     def getAt(List where) {
-      println "getAt: $where"
+      //println "getAt: $where"
       Integer rowIdx = where[0] as Integer
       Integer colIdx = where[1] as Integer
       return rows[rowIdx][colIdx]
@@ -102,9 +102,9 @@ class PutAtTest {
   @Test
   void testOverride() {
     def d = new TwoD([[1,2,3], ['a', 'b', 'c']])
-    println d
+    //println d
     d[0,1] = null
-    println d
+    //println d
     assert d[0,1] == null
     assert d[1,2] == 'c'
   }
@@ -126,6 +126,35 @@ class PutAtTest {
     //println(empData.content())
     assert empData[1, 0] == null
     assert empData[1, 1] == "Dan"
+    empData["salary"][0] = null
+    assert empData["salary"][0] == null
+
+    assert 5 == empData.rowCount()
+    assert 4 == empData.columnCount()
+  }
+
+  @Test
+  void testAssignNullToMatrixElementDynamic() {
+    def empData = Matrix.builder()
+        .matrixName('empData')
+        .data(
+            emp_id: 1..5,
+            emp_name: ["Rick", "Dan", "Michelle", "Ryan", "Gary"],
+            salary: [623.3, 515.2, 611.0, 729.0, 843.25],
+            start_date: toLocalDates("2012-01-01", "2013-09-23", "2014-11-15", "2014-05-11", "2015-03-27")
+        )
+        .types([int, String, Number, LocalDate])
+        .build()
+    empData[1, 0] = null
+    //println(empData.content())
+    assert empData[1, 0] == null
+    assert empData[1, 1] == "Dan"
+    empData["salary"][0] = null
+    assert empData["salary"][0] == null
+    // This only works with dynamic compile
+    empData.salary[1] = null
+    assert empData.salary[1] == null
+
     assert 5 == empData.rowCount()
     assert 4 == empData.columnCount()
   }
