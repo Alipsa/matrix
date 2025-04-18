02dd5e90d3750a70cb47bd47f2858c0d98f692f4
per <per@alipsa.se>
2025-04-18 19:20:52 +0200

    windows fixes

--- release.sh
+++ release.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
+set -e
 source ~/.sdkman/bin/sdkman-init.sh
 source jdk21
-./gradlew clean publishToSonatype closeAndReleaseSonatypeStagingRepository
+
+function release() {
+   pushd "$1"
+   ./release.sh
+   popd
+}
+#release matrix-core
+#release matrix-datasets
+release matrix-stats
+release matrix-sql
+release matrix-spreadsheet
+release matrix-json
+release matrix-csv
+release matrix-parquet
+release matrix-bigquery
+release matrix-charts
+release matrix-xchart
+release matrix-tablesaw
+pushd matrix-bom
+    mvn -DstagingProcessTimeoutMinutes=10 -Prelease -f bom.xml clean deploy
+popd
+echo "done"
