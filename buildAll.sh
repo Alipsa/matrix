57ee1fbec90f339df2fb4b2f74052e51e742c8d8
pernyf <per.nyfelt@nordnet.se>
2025-06-02 11:44:17 +0200

    fix build order

--- buildAll.sh
+++ buildAll.sh
@@ -5,10 +5,14 @@ if [[ -d "$localRepo/se/alipsa/matrix" ]]; then
   echo "Removing local cache in $localRepo/se/alipsa/matrix"
   rm -r "$localRepo/se/alipsa/matrix"
 fi
+echo "Locally publish bom"
+pushd matrix-bom
+  mvn -f bom.xml install
+popd
 echo "Building and locally publishing matrix"
 ./gradlew build publishToMavenLocal
-echo "Locally publish bom and verify"
-cd matrix-bom
-mvn -f bom.xml install
-mvn verify
+echo "Verify bom"
+pushd matrix-bom
+  mvn verify
+popd
 echo "Done!"
