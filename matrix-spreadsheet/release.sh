49d3480c0495ece9ebaca5a3f1045c9babdd3132
per <per@alipsa.se>
2025-07-20 00:04:26 +0200

    release matrix-spreadsheet 2.2.1

--- matrix-spreadsheet/release.sh
+++ matrix-spreadsheet/release.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 source ~/.sdkman/bin/sdkman-init.sh
 source jdk21
 #./gradlew clean publishToSonatype closeAndReleaseSonatypeStagingRepository
@@ -7,7 +8,7 @@ PROJECT=$(basename "$PWD")
 if grep "version '" build.gradle | grep -q 'SNAPSHOT'; then
   echo "$PROJECT snapshot published"
 else
-  echo "$PROJECT uploaded, release it if it checks out"
-  echo "see https://central.sonatype.org/publish/release/ for more info"
+  echo "$PROJECT published"
+  echo "see https://central.sonatype.com/publishing/deployments for more info"
   # browse https://oss.sonatype.org &
 fi
