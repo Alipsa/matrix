9dda67c30114bb77d6c48a496b9501dcdb8598ba
per <per@alipsa.se>
2026-01-30 23:15:36 +0100

    release arff 0.1.0

---
+++ matrix-arff/release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+source ~/.sdkman/bin/sdkman-init.sh
+source jdk21
+#./gradlew clean publishToSonatype closeAndReleaseSonatypeStagingRepository
+../gradlew -PrunExternalTests=true :matrix-arff:clean :matrix-arff:build :matrix-arff:release || exit 1
+PROJECT=$(basename "$PWD")
+if grep "version '" build.gradle | grep -q 'SNAPSHOT'; then
+  echo "$PROJECT snapshot published"
+else
+  echo "$PROJECT uploaded, release it if it checks out"
+  echo "see https://central.sonatype.com/publishing/deployments for more info"
+  # browse https://oss.sonatype.org &
+fi
