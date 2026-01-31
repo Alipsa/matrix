5e1dd50f32301305df2398d753ebba63e67ec8ec
per <per@alipsa.se>
2026-01-31 18:20:14 +0100

    release spreadsheet 2.3.0

--- matrix-spreadsheet/release.sh
+++ matrix-spreadsheet/release.sh
@@ -2,8 +2,9 @@
 set -e
 source ~/.sdkman/bin/sdkman-init.sh
 source jdk21
-#./gradlew clean publishToSonatype closeAndReleaseSonatypeStagingRepository
-./gradlew :matrix-spreadsheet:clean :matrix-spreadsheet:build :matrix-spreadsheet:release || exit 1
+./gradlew :matrix-spreadsheet:clean :matrix-spreadsheet:build
+echo "Build was successful, publishing to maven central..."
+./gradlew :matrix-spreadsheet:release
 PROJECT=$(basename "$PWD")
 if grep "version '" build.gradle | grep -q 'SNAPSHOT'; then
   echo "$PROJECT snapshot published"
