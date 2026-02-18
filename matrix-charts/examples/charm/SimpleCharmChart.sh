9ba26496737a66a5c87b504307edc28fdb723894
per <per@alipsa.se>
2026-02-18 21:18:43 +0100

    refine example script

--- matrix-charts/examples/charm/SimpleCharmChart.sh
+++ matrix-charts/examples/charm/SimpleCharmChart.sh
@@ -3,9 +3,11 @@
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/../.."
+echo "Building matrix-charts project..."
 ./gradlew build -x test
 CLASSPATH=$(./gradlew -q printCp)
 cd "$SCRIPT_DIR"
-echo "Using CLASSPATH: $CLASSPATH"
+# echo "Using CLASSPATH: $CLASSPATH"
+echo "Running SimpleCharmChart.groovy..."
 groovy -cp "$CLASSPATH" SimpleCharmChart.groovy
 
