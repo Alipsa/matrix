1680dbf71fc493457b7c620536cb489f1f5e748b
per <per@alipsa.se>
2026-06-30 22:32:20 +0200

    Add release script

--- matrix-ggplot/release.sh
+++ matrix-ggplot/release.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+echo "Releasing project in $SCRIPT_DIR"
+cd "$SCRIPT_DIR" || exit 1
+
+# shellcheck disable=SC1090
 source ~/.sdkman/bin/sdkman-init.sh
 source jdk21
 
 PROJECT=$(basename "$PWD")
 
-../gradlew :$PROJECT:clean :$PROJECT:build :$PROJECT:release || exit 1
+../gradlew :"$PROJECT":clean :"$PROJECT":build :"$PROJECT":release || exit 1
 
 if grep "version '" build.gradle | grep -q 'SNAPSHOT'; then
   echo "$PROJECT snapshot published"
