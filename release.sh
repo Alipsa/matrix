ee55013d7ae117eab84ba4f122989d6fefdbe7c9
per <per@alipsa.se>
2026-09-11 22:54:39 +0200

    update release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 source ~/.sdkman/bin/sdkman-init.sh
-source jdk21
+source jdk 21
 
 # Enable external tests such a BigQuery and Gsheets
 export RUN_EXTERNAL_TESTS=true
@@ -9,7 +9,12 @@ export RUN_SLOW_TESTS=true
 
 function release() {
    pushd "$1"
-   ./release.sh
+   version=$(./gradlew -q printVersion)
+   if [[ $version == *-SNAPSHOT ]]; then
+      echo "Module $(basename "$PWD") version is a snapshot ($version), skipping..."
+   else
+      ./release.sh
+   fi
    popd
 }
 ./gradlew spotlessApply
@@ -17,18 +22,28 @@ if [ -n "$(git status --porcelain)" ]; then
   echo "Error: Git working tree is not clean. Commit or stash changes before publishing."
   exit 1
 fi
+release matrix-groovy-ext
+release matrix-logging
 release matrix-core
-release matrix-datasets
 release matrix-stats
+release matrix-datasets
 release matrix-sql
 release matrix-spreadsheet
+release matrix-gsheets
 release matrix-json
 release matrix-csv
+release matrix-arff
+release matrix-avro
+release matrix-json
 release matrix-parquet
 release matrix-bigquery
+release matrix-smile
 release matrix-charts
+release matrix-pict
+release matrix-ggplot
 release matrix-xchart
 release matrix-tablesaw
+release matrix-jupyter
 pushd matrix-bom
     mvn -DstagingProcessTimeoutMinutes=10 -Prelease -f bom.xml clean deploy
 popd
