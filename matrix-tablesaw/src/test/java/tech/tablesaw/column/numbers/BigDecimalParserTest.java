6a745a216d5c35b67f5c03a00a36bdcdb255cff3
per <per@alipsa.se>
2026-09-18 22:40:11 +0200

    Initialise BigDecimalColumnType.INSTANCE before DEFAULT_PARSER

--- matrix-tablesaw/src/test/java/tech/tablesaw/column/numbers/BigDecimalParserTest.java
+++ matrix-tablesaw/src/test/java/tech/tablesaw/column/numbers/BigDecimalParserTest.java
@@ -1,15 +1,23 @@
 package tech.tablesaw.column.numbers;
 
 import org.junit.jupiter.api.Test;
+import tech.tablesaw.api.BigDecimalColumn;
 import tech.tablesaw.io.xml.XmlReadOptions;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BigDecimalParserTest {
+
+  @Test
+  public void defaultParserKnowsItsColumnType() {
+    assertSame(BigDecimalColumnType.instance(), BigDecimalColumnType.DEFAULT_PARSER.columnType());
+    assertSame(BigDecimalColumnType.instance(), BigDecimalColumn.create("c").parser().columnType());
+  }
 
   @Test
   public void testMissingIndicatorsAreTreatedAsMissing() {
